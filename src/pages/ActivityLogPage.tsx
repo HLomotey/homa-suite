2357ae686d60c232548003b853f827dfdadcfbd1
HLomotey <51993991+HLomotey@users.noreply.github.com>
2025-08-24 15:23:31 +0000

    feat: add ActivityLogPage component with dashboard integration

--- src/pages/ActivityLogPage.tsx
+++ src/pages/ActivityLogPage.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { ActivityLogDashboard } from '@/components/activity-log/ActivityLogDashboard';
+import React from "react";
+import { ActivityLogDashboard } from "@/components/activity-log/ActivityLogDashboard";
 
 const ActivityLogPage: React.FC = () => {
   return (
-    <div className="container mx-auto px-4 py-6">
+    <div className="space-y- p-6">
       <ActivityLogDashboard />
     </div>
   );
